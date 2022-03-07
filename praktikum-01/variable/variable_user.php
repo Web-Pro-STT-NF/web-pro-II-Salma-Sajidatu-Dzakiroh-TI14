79fc87ced7aa720d4fdd4658e1b6ad197dc54f90
salmasdz <92497179+Salmasajidatu@users.noreply.github.com>
2022-03-07 10:18:47 +0700

    variabel

---
+++ praktikum-01/variable/variable_user.php
@@ -0,0 +1,17 @@
+<?php 
+
+// membuat variabel
+//$nameVaribel = nilaiVariabel;
+$name = "Salma Sajidatu Dzakiroh";
+$age = 20;
+$weight = 45.5;
+
+// menampilkan variabel
+// echo "isi yang ingin ditampilkan";
+echo "Name : " . $name;
+echo "<br>Age : $age";
+echo "<br>Weight : $weight";
+
+echo "<br/>Hello $name Apa Kabar";
+
+?>
